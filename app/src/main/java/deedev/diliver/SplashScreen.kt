20b2d08aea259a9bf16dd1787628cbdcfc548d60
Deepansh Gupta <deepansh.gup124@gmail.com>
2024-11-12 01:16:34 +0530

    Deepansh

---
+++ app/src/main/java/deedev/diliver/SplashScreen.kt
@@ -0,0 +1,22 @@
+package deedev.diliver
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.logging.Handler
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        android.os.Handler().postDelayed({
+            val intent= Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
+
---
+++ app/src/main/java/deedev/diliver/StartActivity.kt
@@ -0,0 +1,5 @@
+package deedev.diliver
+
+class StartActivity {
+
+}
