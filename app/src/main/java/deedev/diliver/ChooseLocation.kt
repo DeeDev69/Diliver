ed4a738f7aa24013a69e4076952c0cba4cc0b9d5
Deepansh Gupta <deepansh.gup124@gmail.com>
2024-11-13 10:17:48 +0530

    Deepansh

--- app/src/main/java/deedev/diliver/ChooseLocation.kt
+++ app/src/main/java/deedev/diliver/ChooseLocation.kt
@@ -1,6 +1,7 @@
 package deedev.diliver
 
 import android.R
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
@@ -8,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import deedev.diliver.databinding.ActivityChooseLocationBinding
+import kotlinx.coroutines.selects.select
 
 class ChooseLocation : AppCompatActivity() {
     private val binding: ActivityChooseLocationBinding by lazy {
@@ -17,13 +19,59 @@ class ChooseLocation : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val locations = arrayOf("Choose Location","Ahmedabad", "Bengaluru", "Bhopal", "Bhubaneswar", "Chandigarh", "Chennai", "Coimbatore", "Dehradun", "Delhi", "Faridabad", "Ghaziabad", "Gurugram", "Guwahati", "Hyderabad", "Indore", "Jaipur", "Jalandhar", "Jammu", "Jodhpur", "Kanpur", "Kochi", "Kolkata", "Lucknow", "Ludhiana", "Madurai", "Mumbai", "Nagpur", "Nashik", "Noida", "Patna", "Pune", "Raipur", "Rajkot", "Ranchi", "Srinagar", "Surat", "Thiruvananthapuram", "Udaipur", "Vadodara", "Varanasi", "Visakhapatnam")
+        val locations = arrayOf(
+            "Choose Location",
+            "Ahmedabad",
+            "Bengaluru",
+            "Bhopal",
+            "Bhubaneswar",
+            "Chandigarh",
+            "Chennai",
+            "Coimbatore",
+            "Dehradun",
+            "Delhi",
+            "Faridabad",
+            "Ghaziabad",
+            "Gurugram",
+            "Guwahati",
+            "Hyderabad",
+            "Indore",
+            "Jaipur",
+            "Jalandhar",
+            "Jammu",
+            "Jodhpur",
+            "Kanpur",
+            "Kochi",
+            "Kolkata",
+            "Lucknow",
+            "Ludhiana",
+            "Madurai",
+            "Mumbai",
+            "Nagpur",
+            "Nashik",
+            "Noida",
+            "Patna",
+            "Pune",
+            "Raipur",
+            "Rajkot",
+            "Ranchi",
+            "Srinagar",
+            "Surat",
+            "Thiruvananthapuram",
+            "Udaipur",
+            "Vadodara",
+            "Varanasi",
+            "Visakhapatnam"
+        )
         val spinnerAdapter = ArrayAdapter(this, R.layout.simple_spinner_item, locations)
         spinnerAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
         binding.locationSpinner.adapter = spinnerAdapter
 
-
-
+        binding.chooseLocButton.setOnClickListener{
+            val intent= Intent(this,LoginPage::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 
 }
