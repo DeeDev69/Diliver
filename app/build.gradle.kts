3c5a8d8ceebf430507e4ef741d32de53b5253871
Deepansh Gupta <deepansh.gup124@gmail.com>
2024-11-14 18:12:04 +0530

    Deepansh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,8 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.database)
     implementation(libs.firebase.inappmessaging.display)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/deedev/diliver/MainActivity.kt
+++ app/src/main/java/deedev/diliver/MainActivity.kt
@@ -3,10 +3,13 @@ package deedev.diliver
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        var NavController:NavController=findNavController(R.id.fragmentContainerView)
     }
 }
